4e0fcecf8a8e26285b798d45256989981d0297ac
Javier Callejón <javicallejon88@gmail.com>
2022-05-22 18:42:08 +0200

    Iniciando repositorio

---
+++ db/config.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+// Realizaremos la petición de conexión de forma asincrona
+// controlada por si ocurriese cualquier tipo de error
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Base de datos en cluster ONLINE");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error a la hora de iniciar la BBDD");
+  }
+};
+
+module.exports = {
+  dbConnection,
+};
