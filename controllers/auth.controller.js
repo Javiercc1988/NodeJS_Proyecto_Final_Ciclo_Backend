7fdc1be628e466485d175a8853136b1271689750
Javier Callejón <javicallejon88@gmail.com>
2022-05-24 16:21:48 +0200

    Controllers 50%

---
+++ controllers/auth.controller.js
@@ -0,0 +1,106 @@
+const { response, json } = require("express");
+const bcryptjs = require("bcryptjs");
+
+const Usuario = require("../models/user");
+const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
+const { DefaultTransporter } = require("google-auth-library");
+
+const login = async (req, res = response) => {
+  const { correo, password } = req.body;
+
+  try {
+    // Verificar si el email existe
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - correo",
+      });
+    }
+
+    // Verificar si el usuario está activo
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - estado: false",
+      });
+    }
+    // Verificar la contraseña
+    const validPassword = bcryptjs.compareSync(password, usuario.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - password",
+      });
+    }
+
+    // Generar el JWT(Json web token)
+    if (usuario.rol === "ADMIN_ROLE") {
+      const token = await generarJWT(usuario.id);
+      res.json({
+        usuario,
+        token,
+      });
+    } else {
+      res.json({
+        usuario,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Algo ha salido mal",
+    });
+  }
+};
+
+/*****************************************/
+
+const googleSignIn = async (req, res) => {
+  const { id_token } = req.body;
+
+  try {
+    const { nombre, img, correo } = await googleVerify(id_token);
+
+    let usuario = await Usuario.findOne({ correo });
+
+    if (!usuario) {
+      // Si el usuario no existe, lo creamos
+      const data = {
+        nombre,
+        correo,
+        password: ".....",
+        img,
+        google: true,
+        rol: "USER_ROLE",
+      };
+
+      usuario = new Usuario(data);
+      await usuario.save();
+    }
+
+    // Si el usuario en nuestra bbdd está 'borrado' es decir estado=false
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: "Hable con el administrador, usuario bloqueado",
+      });
+    }
+
+    // Generar el JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      usuario,
+      token,
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: "El token no se ha podido verificar.",
+    });
+  }
+};
+
+module.exports = {
+  login,
+  googleSignIn,
+};
