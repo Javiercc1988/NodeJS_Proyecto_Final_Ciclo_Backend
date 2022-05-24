bbd80dbfec6be98b0e5327f4e4f5f1be927cb289
Javier Callejón <javicallejon88@gmail.com>
2022-05-24 17:11:53 +0200

    Controllers

---
+++ controllers/users.controller.js
@@ -0,0 +1,92 @@
+const { response } = require("express");
+const bcryptjs = require("bcryptjs");
+
+const User = require("../models/user");
+
+// USER GET
+const usersGet = async (req, res = response) => {
+  const { limite = 5, desde = 0 } = req.query;
+  const query = { estado: true };
+  // Solo nos interesan los usuarios con estado true, para saber que no son usuarios 'borrados' de la bbdd
+
+  const [total, usuarios] = await Promise.all([
+    User.countDocuments(query),
+    User.find(query).skip(Number(desde)).limit(Number(limite)),
+  ]);
+  // Hacemos una desestructuración de array y utilizamos Promise.all para realizar las query simultaneamente recortando mucho el tiempo de la consulta.
+
+  res.json({ total, usuarios });
+};
+
+// USER POST
+const usersPost = async (req, res) => {
+  const rol = req.body.rol || "USER_ROLE";
+  const { nombre, apellidos, dni, correo, password, telefono } = req.body;
+  const user = new User({
+    nombre,
+    apellidos,
+    dni,
+    correo,
+    password,
+    rol,
+    telefono,
+  });
+
+  // Encriptación de contraseña de usuario
+  const salt = bcryptjs.genSaltSync();
+  user.password = bcryptjs.hashSync(password, salt);
+
+  // Guardar el registro en la BBDD
+  await user.save();
+
+  res.json({
+    user,
+  });
+};
+
+// USER PUT
+const usersPut = async (req, res) => {
+  const { id } = req.params;
+  const { _id, password, google, correo, ...resto } = req.body;
+
+  //Validación contra la BBDD
+  if (password) {
+    const salt = bcryptjs.genSaltSync();
+    resto.password = bcryptjs.hashSync(password, salt);
+  }
+
+  const user = await User.findByIdAndUpdate(id, resto, { new: true });
+
+  res.json(user);
+};
+
+// USER PATCH
+const usersPatch = (req, res) => {
+  res.json({
+    msg: "Patch API - Controller",
+  });
+};
+
+// USER DELETE
+const usersDelete = async (req, res) => {
+  const { id } = req.params;
+
+  // Borrado fisicamente
+  // const usuario = await User.findByIdAndDelete(id);
+
+  // Para evitar perdida referencial en la bbdd en lugar de borrar fisicamente lo que hacemos es cambiar el estado
+  // Esta es la forma más recomendada.
+  const usuario = await User.findByIdAndUpdate(id, { estado: false });
+
+  res.json({
+    usuario,
+  });
+};
+
+module.exports = {
+  usersGet,
+  usersPost,
+  usersPut,
+  usersPatch,
+  usersDelete,
+};
