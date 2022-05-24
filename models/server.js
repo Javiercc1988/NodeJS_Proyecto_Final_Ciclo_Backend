8bd8604dfea1fdc3e6ab7085a53829074dc06107
Javier Callejón <javicallejon88@gmail.com>
2022-05-24 16:18:24 +0200

    models

---
+++ models/server.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("../db/config");
+const fileUpload = require("express-fileupload");
+
+class Server {
+  // Cuando llamemos al constructor al final vamos a llamar las rutas.
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    this.paths = {
+      auth: "/api/auth",
+      buscar: "/api/buscar",
+      categorias: "/api/categorias",
+      users: "/api/users",
+      productos: "/api/productos",
+      uploads: "/api/uploads",
+    };
+
+    // Conectar a base de datos
+    this.connectDB();
+
+    // Middlewares
+    this.middlewares();
+
+    // Rutas de mi aplicación
+    this.routes();
+  }
+
+  async connectDB() {
+    await dbConnection();
+  }
+
+  middlewares() {
+    // Acceso al directorio público
+    this.app.use(express.static("public"));
+
+    // CORS
+    this.app.use(cors());
+
+    // Lectura del body recibido
+    this.app.use(express.json());
+
+    // FileUpload - Carga de archivos
+    this.app.use(
+      fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+        createParentPath: true,
+      })
+    );
+  }
+
+  // Creamos los métodos
+  routes() {
+    this.app.use(this.paths.auth, require("../routes/auth.routes"));
+    this.app.use(this.paths.buscar, require("../routes/buscar.routes"));
+    this.app.use(this.paths.categorias, require("../routes/categorias.routes"));
+    this.app.use(this.paths.users, require("../routes/user.routes"));
+    this.app.use(this.paths.productos, require("../routes/productos.routes"));
+    this.app.use(this.paths.uploads, require("../routes/uploads.routes"));
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor funcionando en el puerto", this.port);
+    });
+  }
+}
+
+module.exports = Server;
---
+++ models/user.js
@@ -0,0 +1,69 @@
+/* Mongo, a diferencia de las bbdd relacionales se graba en objetos { objetos: objetos } también conocidos como 'documento', los documentos se graban dentro de colecciones
+y las colecciones para los que vienen de bbdd relacionales son como las tablas.*/
+
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+  nombre: {
+    type: String,
+    required: [true, "El nombre es obligatorio."],
+    // Podemos mandar un array para indicar un mensaje de error además de que es requerido
+  },
+  apellidos: {
+    type: String,
+    required: [true, "Los Apellidos son obligatorios."],
+    // Podemos mandar un array para indicar un mensaje de error además de que es requerido
+  },
+  dni: {
+    type: String,
+    required: [true, "El DNI es obligatorio."],
+    unique: true,
+    // Podemos mandar un array para indicar un mensaje de error además de que es requerido
+  },
+  telefono: {
+    type: String,
+    required: [true, "El Telefono es obligatorio."],
+    // Podemos mandar un array para indicar un mensaje de error además de que es requerido
+  },
+  correo: {
+    type: String,
+    required: [true, "El correo es obligatorio."],
+    unique: true,
+    // No es posible repetir emails.
+  },
+  password: {
+    type: String,
+    require: [true, "La contraseña es obligatoria."],
+  },
+  img: {
+    type: String,
+  },
+  rol: {
+    type: String,
+    required: true,
+    enum: ["ADMIN_ROLE", "USER_ROLE"],
+  },
+  estado: {
+    type: Boolean,
+    default: true,
+  },
+  google: {
+    type: Boolean,
+    default: false,
+    // Este campo nos indicará si el usuario ha sido creado por google.
+  },
+});
+
+//************************************************//
+// Vamos a personalizar el metodo toJSON de mongoose. Nos va a permitir decidir
+// que es lo que se muestra de nuestro modelo, de esta forma evitaremos que nos muestre
+// tanto la versión (__v) como la contraseña.
+
+UserSchema.methods.toJSON = function () {
+  const { __v, password,_id, ...user } = this.toObject();
+  user.uid = _id
+  // Esto nos va a generar nuestra instancia pero con nuestros valores respectivos
+  return user;
+};
+
+module.exports = model("User", UserSchema);
